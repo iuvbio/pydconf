b2c98354d70e5cc0d0144f0c7b4ff7374dd1cfa7
iuvbio <lyndonbjohn@gmail.com>
2020-03-12 00:29:40 +0100

    add requirements and setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import find_packages, setup
+
+import pytermconf
+
+
+version = pytermconf.__version__
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    dependencies = [l.strip() for l in f.readlines() if not l.startswith('#')]
+
+setup(
+    name='pytermconf',
+    version=version,
+    author='iuvbio',
+    author_email='cryptodemigod@protonmail.com',
+    description='Generates gnome-terminal profiles based on an image',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    keywords="""\
+    colorscheme gnome-terminal profiles terminal-emulators \
+    changing-colorschemes\
+    """,
+    packages=find_packages(),
+    entry_points={'console_scripts': ['pytc=pytermconf.main:cli']},
+    install_requires=dependencies,
+    extras_require={
+        'imgpal': []
+    }
+)
