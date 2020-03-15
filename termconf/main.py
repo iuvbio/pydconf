fd9fcdba344d982da344a7b3346e4ba3758c0cc2
iuvbio <lyndonbjohn@gmail.com>
2020-03-15 04:58:33 +0100

    pass common arguments

--- termconf/main.py
+++ termconf/main.py
@@ -9,6 +9,24 @@ from .backends import *
 from . import termconf
 
 
+class Config:
+    def __init__(self):
+        self.verbose = 0
+        self.backend = 'imgpal'
+        self.set_default = False
+
+    @property
+    def level(self):
+        return {
+            0: 'WARNING',
+            1: 'INFO',
+            2: 'DEBUG'
+        }.get(self.verbose)
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
 def get_colors(image, backend):
     backend = sys.modules[f"termconf.backends.{backend}"]
     colors = backend.get(image)
@@ -22,46 +40,45 @@ def get_colors(image, backend):
     return colors
 
 
-@click.group()  # TODO: look up click pass config
+@click.group()
 @click.option('-v', '--verbose', count=True, default=0)
-def cli(verbose):
-    level = {
-        0: 'WARNING',
-        1: 'INFO',
-        2: 'DEBUG'
-    }.get(verbose)
-    logging.basicConfig(level=level)
+@click.option('--backend', default='imgpal', type=click.Choice(['imgpal', 'wal']))
+@click.option('--set-default', is_flag=True)
+@pass_config
+def cli(config, verbose, backend, set_default):
+    config.verbose = verbose
+    logging.basicConfig(level=config.level)
+    config.backend = backend
+    config.set_default = set_default
 
 
 @click.command()
 @click.argument('image', type=click.Path(exists=True))
 @click.option('-n', '--name')
-@click.option('--backend', default='imgpal', type=click.Choice(['imgpal', 'wal']))
-@click.option('--set-default', is_flag=True)
-def make_profile_from_image(image, name, backend, set_default):
+@pass_config
+def make_profile_from_image(config, image, name):
     if not name:
         name = Path(image).stem
-    profile_params = get_colors(image, backend)
+    profile_params = get_colors(image, config.backend)
     profile_params.update({
         'bold-color-same-as-fg': 'false',
         'use-theme-colors': 'false',
         'use-theme-background': 'false'
     })
-    termconf.create_new_profile(name, profile_params, set_default=set_default)
+    termconf.create_new_profile(name, profile_params, set_default=config.set_default)
 
 
 @click.command()
 @click.argument('name')
 @click.argument('image', type=click.Path(exists=True))
-@click.option('-b', '--backend', default='imgpal')
-@click.option('--set-default', is_flag=True)
-def set_profile_from_image(name, image, backend, set_default):
+@pass_config
+def set_profile_from_image(config, name, image):
     profile_id = termconf.get_profile_id(name)
-    profile_params = get_colors(image, backend)
+    profile_params = get_colors(image, config.backend)
     termconf.set_profile_key(profile_id, 'visible-name', name)
     for key, value in profile_params.items():
         termconf.set_profile_key(profile_id, key, value)
-    if set_default:
+    if config.set_default:
         termconf.set_default_profile(profile_id)
 
 
